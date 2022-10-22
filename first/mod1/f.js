f6af6d8d0312058d832ee5f77bbbca013334e469
Aman Kumar Pandey <amanpandey231217@gmail.com>
2022-10-22 12:30:42 +0530

    Create f.js

---
+++ first/mod1/f.js
@@ -0,0 +1,54 @@
+const {
+  start_f,
+  stop_f,
+  err_queue,
+  task_done,
+} = require("./queue_init");
+const task1 = require("./structure/task1");
+
+start_f.process((job) => {
+  task1
+    .findByIdAndUpdate(job.data.id, {
+      $set: {
+        pid: job.data.pid,
+        status: "Running",
+      },
+    })
+    .then()
+    .catch((err) => console.log({ err }));
+});
+
+stop_f.process((job) => {
+  console.log(job.data.id);
+  task1
+    .findByIdAndUpdate(job.data.id, {
+      $set: {
+        status: "terminated",
+      },
+    })
+    .then((e) => console.log(`Job Stopped ${job.data.id}`))
+    .catch((err) => console.log({ err }));
+});
+
+task_done.process((job) => {
+  task1
+    .findByIdAndUpdate(job.data.id, {
+      $set: {
+        status: "task completed",
+      },
+    })
+    .then(() => console.log("Job completed"))
+    .catch((err) => console.log({ err }));
+});
+
+err_queue.process((job) => {
+  console.log(job.data.error);
+  task1
+    .findByIdAndUpdate(job.data.id, {
+      $set: {
+        status: `error:${job.data.error}`,
+      },
+    })
+    .then()
+    .catch((err) => console.log(err));
+});
