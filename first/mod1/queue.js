e6a3999afa7ffd9d42a326fd198f5b976cd0bd02
Aman Kumar Pandey <amanpandey231217@gmail.com>
2022-10-22 12:34:22 +0530

    Create queue.js

---
+++ first/mod1/queue.js
@@ -0,0 +1,11 @@
+const Queue = require("bull");
+const redis_url1 = process.env.broker_url;
+
+module.exports = {
+  start_queue1: new Queue("start", redis_url1),
+  stop_queue1: new Queue("stop", redis_url1),
+  start_f: new Queue("start_f", redis_url1),
+  stop_f: new Queue("stop_f", redis_url1),
+  err_queue1: new Queue("err_feedback", redis_url1),
+  task_completion: new Queue("task_done", redis_url1),
+};
